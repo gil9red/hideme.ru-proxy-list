0d5f6dd1b36b1976e42b45cf2702fb277754f97c
Ilya Petrash <ilya.petrash@inbox.ru>
2015-08-20 00:23:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -35,14 +35,30 @@ def get_logger(name, file='log.txt', encoding='utf8'):
     return log
 
 
+from random import randint, choice
+
+USER_AGENTS = [
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:39.0) Gecko/20100101 Firefox/39.0',
+    'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5',
+]
+
+# Притворяемся браузером
+HEADERS = {
+    'User-agent': choice(USER_AGENTS),
+    'Accept': 'text/xml,application/xml,application/xhtml+xml,text/html;'
+              'q=0.9,text/plain;q=0.8,image/png,*/*;q=0.%d' % randint(2, 5),
+    'Accept-Language': 'en-us,en;q=0.%d' % randint(5, 9),
+    'Accept-Charset': 'utf-8,windows-1251;q=0.7,*;q=0.%d' % randint(5, 7),
+    'Keep-Alive': '300',
+    'Expect': '',
+}
+
+
 def go_url(url):
     """Функция возвращает страницу url в виде байтового массива."""
 
     # Добавляем заголовок, чтобы hideme.ru не посчитал нас ботом
-    # Притворяемся браузером
-    rq = Request(url)
-    rq.add_header('User-agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.1.5) '
-                                'Gecko/20091102 Firefox/3.5.5')
+    rq = Request(url, headers=HEADERS)
 
     logger.debug('Перехожу по url: %s.', url)
 
@@ -75,6 +91,51 @@ if __name__ == '__main__':
 
     logger.info('Страница загружена.')
 
+
+    from lxml import etree
+
+    # Вытаскиваем список строк таблицы с атрибутом class="pl", у строк должны быть вложены теги td
+    xpath = '//table[@class="pl"]/tr[td]'
+    # print(data.decode('cp1251'))
+    tree = etree.HTML(data.decode('cp1251'))
+    rows = tree.xpath(xpath)
+
+    print(len(rows), rows)
+
+
+# from lxml import etree
+#
+# tree = etree.XML(xml) # Парсинг строки
+# #tree = etree.parse('1.xml') # Парсинг файла
+#
+# nodes = tree.xpath('/soft/os/item') # Открываем раздел
+# for node in nodes: # Перебираем элементы
+#     print node.tag,node.keys(),node.values()
+#     print 'name =',node.get('name') # Выводим параметр name
+#     print 'text =',[node.text] # Выводим текст элемента
+#
+# # Доступ к тексту напрямую, с указанием фильтра
+# print 'text1',tree.xpath('/soft/os/item[@name="linux"]/text()')
+# print 'text2',tree.xpath('/soft/os/item[2]/text()')
+# # Доступ к параметру напрямую
+# print 'dist',tree.xpath('/soft/os/item[@name="linux"]')[0].get('dist')
+# # Выборка по ключу
+# print 'dist by key',tree.xpath('//*[@name="windows"]')[0].get('dist')
+#
+# print 'iterfind:'
+# for node in tree.iterfind('.//item'): # поиск элементов
+#     print node.get('name')
+#
+# # Рекурсивный перебор элементов
+# print 'recursiely:'
+# def getn(node):
+#     print node.tag,node.keys()
+#     for n in node:
+#         getn(n)
+# getn(tree.getroottree().getroot())
+
+    quit()
+
     # TODO: полностью заменить grab на lxml
     g = Grab(data)
 
@@ -86,7 +147,11 @@ if __name__ == '__main__':
     logger.debug('Начинаю парсить загруженную страницу.\n')
 
     try:
-        for row in g.doc.select(xpath):
+        rows = g.doc.select(xpath)
+        if len(rows) == 0:
+            raise Exception('Список прокси пустой')
+
+        for row in rows:
             logger.debug('Начинаю разбирать строку с прокси.')
             ip, port, country, city, speed, proxy_type, anonymity, checked = row.select('td')
 
@@ -125,9 +190,13 @@ if __name__ == '__main__':
     logger.debug('Закончен разбор. Найдено %s прокси.', len(proxy_list))
 
     out = 'proxy-list.txt'
-    logger.debug('Сохраняю прокси в файл: %s.', out)
 
-    # Сохраним найденные прокси в файл
-    with open(out, mode='w') as f:
-        for proxy in proxy_list:
-            f.write('{0.ip}:{0.port}\n'.format(proxy))
+    if proxy_list:
+        logger.debug('Сохраняю прокси в файл: %s.', out)
+
+        # Сохраним найденные прокси в файл
+        with open(out, mode='w') as f:
+            for proxy in proxy_list:
+                f.write('{0.ip}:{0.port}\n'.format(proxy))
+    else:
+        logger.warn('Список прокси пустой, отмена сохранения.')
